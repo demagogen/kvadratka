6b7b25503415baa59e5cb4fedc2c22625601f871
demagogen <roman.korennoy@gmail.com>
2024-08-14 22:36:08 +0300

    Поменял суффикс
    Поменял суффикс с .c на .cpp по рекомендации Деда

--- kvadratka.cpp
+++ kvadratka.cpp
@@ -8,38 +8,55 @@ int main() {
     double x1, x2; //корни
     double discriminant;
     a = b = c = x1 = x2 = discriminant = NAN;
+
     printf("Введите коэффициенты квадратного уравнения\n");
     printf("Первый: ");
     scanf("%lf", &a);
+
     printf("Второй: ");
     scanf("%lf", &b);
+
     printf("Третий: ");
     scanf("%lf", &c);
 
     if (fabs(a) < ZERO) {
+
         if (fabs(b) < ZERO) {
-            printf("Нет корней");
+
+            if (fabs(c) < ZERO) {
+                printf("Решение уравнения -- множество ");
+                printf("действительных чисел");
+            }
+
+            else {
+                printf("Нет корней");
+            }
         }
+
         else {
             x1 = x2 = -(c/b);
             printf("Один корень: %lf", x1);
         }
     }
+
     else {
         discriminant = b*b - 4*a*c;
+
         if (discriminant >= ZERO) {
             x1 = (-b + sqrt(discriminant))/(2*a);
             x2 = (-b - sqrt(discriminant))/(2*a);
-            printf("Два корня: %lf, %lf", x1, x2);
+            printf("Два корня: %lf %lf", x1, x2);
         }
+
         else if (fabs(discriminant) < ZERO) {
             x1 = x2 = -b/(2*a);
             printf("Один корень: %lf", x1);
         }
+
         else {
             printf("Нет корней");
         }
     }
+
     return 0;
 }
-

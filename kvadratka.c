e5927f62ab5e8e4487dc78bc5ca1d5c993086146
demagogen <roman.korennoy@gmail.com>
2024-08-14 22:27:21 +0300

    Внес изменения 1 -- МТ
    1. Добавил константу const double ZERO = 0.0000001.
    2. Инициализировал переменные.
    3. Воспользовался функцией fabs() в условиях
    4. Учтен случай, когда три коэффициента равны нулю.
    5. Стараюсь улучшить читаемость пробелами

--- kvadratka.c
+++ kvadratka.c
@@ -3,42 +3,60 @@
 #include<math.h>
 
 int main() {
+    const double ZERO = 0.0000001;
     double a, b, c; //коэффициенты
     double x1, x2; //корни
     double discriminant;
+    a = b = c = x1 = x2 = discriminant = NAN;
+
     printf("Введите коэффициенты квадратного уравнения\n");
     printf("Первый: ");
     scanf("%lf", &a);
+
     printf("Второй: ");
     scanf("%lf", &b);
+
     printf("Третий: ");
     scanf("%lf", &c);
 
-    if (a < 0.0000001 && a > -0.0000001) {
-        if (b < 0.0000001 && b > -0.0000001) {
-            printf("Нет корней");
+    if (fabs(a) < ZERO) {
+
+        if (fabs(b) < ZERO) {
+
+            if (fabs(c) < ZERO) {
+                printf("Решение уравнения -- множество ");
+                printf("действительных чисел");
+            }
+
+            else {
+                printf("Нет корней");
+            }
         }
+
         else {
             x1 = x2 = -(c/b);
             printf("Один корень: %lf", x1);
         }
     }
+
     else {
         discriminant = b*b - 4*a*c;
-        if (discriminant >= 0.0000001) {
+
+        if (discriminant >= ZERO) {
             x1 = (-b + sqrt(discriminant))/(2*a);
             x2 = (-b - sqrt(discriminant))/(2*a);
-            printf("Два корня: %lf, %lf", x1, x2);
+            printf("Два корня: %lf %lf", x1, x2);
         }
-        else if (discriminant < 0.0000001 && \
-        discriminant > -0.0000001) {
+
+        else if (fabs(discriminant) < ZERO) {
             x1 = x2 = -b/(2*a);
             printf("Один корень: %lf", x1);
         }
+
         else {
             printf("Нет корней");
         }
     }
+
     return 0;
 }
-

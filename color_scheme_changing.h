ffc6376db7ee4f4b46435e1c81d270dfb05d3c4e
demagogen <roman.korennoy@gmail.com>
2024-08-23 18:39:50 +0300

    add doxyfile

--- color_scheme_changing.h
+++ color_scheme_changing.h
@@ -2,18 +2,20 @@
 #define COLOR_SCHEME_CHANGING_H_
 
 /// @brief Перечислимый тип цветов
+/// @details Перечислимый тип цветов для функции graphic_printf().
 enum COLOR {
-    BLACK   = 0,
-    RED     = 1,
-    GREEN   = 2,
-    YELLOW  = 3,
-    BLUE    = 4,
-    MAGENTA = 5,
-    CYAN    = 6,
-    WHITE   = 7,
+    BLACK   = 0, ///<черный
+    RED     = 1, ///<красный
+    GREEN   = 2, ///<зеленый
+    YELLOW  = 3, ///<желтый
+    BLUE    = 4, ///<голубой
+    MAGENTA = 5, ///<магента
+    CYAN    = 6, ///<бирюзовый
+    WHITE   = 7, ///<белый
 };
 
 /// @brief Перечислимый тип стилей
+/// @details Перечислимый тип стилей для функции graphic_printf().
 enum STYLE {
     RESET         = 0, ///<начальный текст
     BOLD          = 1, ///<включить жирный шрифт
@@ -24,12 +26,16 @@ enum STYLE {
     INVERSE_OFF   = 6, ///<выключить инверсию
 };
 
-/// @brief Меняет цвет и стиль выводящей строки
+/// @brief Меняет цвет и стиль вводимой строки
+/// @details Меняет цвет и стиль вводимой строки из функции
+///          graphic_printf().
 /// @param color цвет
 /// @param style стиль
 void change_color(COLOR color, STYLE style);
 
 /// @brief Выводит строку с цветом и стилем
+/// \details Выводит строку с цветом color и стилей style
+///          в поток вывода.
 /// @param st строка
 /// @param color цвет
 /// @param style стиль
--- solve_square_equation.h
+++ solve_square_equation.h
@@ -2,6 +2,8 @@
 #define SOLVE_SQUARE_EQUATION_H_
 
 /// @brief Перечисляемый тип данных о решениях уравнения
+/// @details Перечислимый тип данных о решениях уравнения,
+///          содержащий информацию о количестве корней.
 enum NUMBER_OF_SOLUTIONS {
     ERROR_NUMBER            = 0,
     NO_SOLUTIONS            = 1,
@@ -11,18 +13,25 @@ enum NUMBER_OF_SOLUTIONS {
 };
 
 /// @brief Структура с данными о решении уравнения
+/// @details Структура с данными о решении уравнения, в
+///          в которую сохраняется информация о решении
+///          квадратного уравнения.
 struct SE_SOLUTIONS{
     double x1, x2;
     NUMBER_OF_SOLUTIONS num_of_sol;
 };
 
 /// @brief Решает линейное уравнение (частный случай)
+/// @details Решает линейное уравнения (частный случай квадратного уравнения)
+///          и сохраняет данные в структуру solutions.
 /// @param b второй коэффициент
 /// @param c третий коэффициент
 /// @param solutions структура с данными о решении уравнения
 void linear_equation      (double b, double c, SE_SOLUTIONS *solutions);
 
-/// @brief Решает квадратное уравнение (частный случай)
+/// @brief Решает квадратное уравнение (общий случай)
+/// @details Решает квадратное уравнение (общий случай) и сохраняет
+///          данные в структуру solutions.
 /// @param a первый коэффициент
 /// @param b второй коэффициент
 /// @param c третий коэффициент
@@ -30,6 +39,9 @@ void linear_equation      (double b, double c, SE_SOLUTIONS *solutions);
 void quadratic_equation   (double a, double b, double c, SE_SOLUTIONS *solutions);
 
 /// @brief Решает квадратное уравнение
+/// @details Решает квадратное уравнение. Рассматривает частные случаи и
+///          запускает функции для решения конкретных случаев квадратного
+///          уравнения.
 /// @param a первый коэффициент
 /// @param b второй коэффициент
 /// @param c третий коэффициент
